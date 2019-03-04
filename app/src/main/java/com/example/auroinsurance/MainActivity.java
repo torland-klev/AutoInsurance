82e17197306967d8f59a44f6c5d8442760cb8b6f
torland-klev <torland-klev@users.noreply.github.com>
2019-03-04 13:57:43 +0100

    Added account checking, won't display buttons for accounts that do not exist

--- app/src/main/java/com/example/auroinsurance/MainActivity.java
+++ app/src/main/java/com/example/auroinsurance/MainActivity.java
@@ -1,17 +1,24 @@
 package com.example.auroinsurance;
 
+import android.accounts.Account;
+import android.accounts.AccountManager;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText username;
     private EditText password;
     private static final int SDK_VERSION = Build.VERSION.SDK_INT;
+    private AccountManager am;
+    private Account[] googleAccounts;
+    private Account[] allAccounts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +27,19 @@ public class MainActivity extends AppCompatActivity {
 
         username = findViewById(R.id.username);
         password = findViewById(R.id.password);
+        am = AccountManager.get(this);
+        googleAccounts = am.getAccountsByType("com.google");
+        allAccounts = am.getAccounts();
+
+        //If no Google Accounts found, remove the button
+        ImageButton ib_google = findViewById(R.id.googleLogo);
+        ImageButton ib_facebook = findViewById(R.id.facebookLogo);
+        if (googleAccounts.length == 0){
+            ib_google.setVisibility(View.INVISIBLE);
+        }
+        if (allAccounts.length == 0) {
+            ib_facebook.setVisibility(View.INVISIBLE);
+        }
 
         //Check SDK-version.
         //Autofill only works for versions 26 and up
@@ -34,5 +54,14 @@ public class MainActivity extends AppCompatActivity {
             Log.i("MainActivity", "API version " +
                     SDK_VERSION + " does not support autofill.\n");
         }
+    }
+
+    public void loginButton(View view) {
+    }
+
+    public void loginGoogle(View view) {
+    }
+
+    public void loginFacebook(View view) {
     }
 }
