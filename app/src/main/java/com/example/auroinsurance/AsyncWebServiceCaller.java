2bb15c0bc50bc7a8016a0e8ff8bb4625346efd20
torland-klev <torland-klev@users.noreply.github.com>
2019-03-05 14:46:33 +0100

    Added some comments

--- app/src/main/java/com/example/auroinsurance/AsyncWebServiceCaller.java
+++ app/src/main/java/com/example/auroinsurance/AsyncWebServiceCaller.java
@@ -13,19 +13,19 @@ import java.util.Arrays;
 
 public class AsyncWebServiceCaller extends AsyncTask<String, Void, String> {
 
-    private final String NAMESPACE = "http://pt.ulisboa.tecnico.sise.autoinsure.ws/";
-    private final String URL = "http://10.0.2.2:8080/AutoInSureWS?WSDL";
-    private static final String serviceName = "AutoInsureWS";
-    public AsyncResponse delegate = null;
+
+    AsyncResponse delegate = null; //Package private
 
     @Override
     protected String doInBackground(String... params) {
         String s;
+
+        //Assumes METHOD is first param, rest of params are specific to a method
         String[] rest = Arrays.copyOfRange(params, 1, params.length );
         try {
             s = makeRequest(params[0], rest);
         } catch (Exception e){
-            s = "Call did not work";
+            s = "Call did not work.\n";
             e.printStackTrace();
         }
         Log.i("AWSC.doInBackground", s);
@@ -37,6 +37,9 @@ public class AsyncWebServiceCaller extends AsyncTask<String, Void, String> {
     }
 
     private String makeRequest(String method, String... args) throws Exception{
+
+        //Using SOAP for making requests as WebServer is WDSL.
+        String NAMESPACE = "http://pt.ulisboa.tecnico.sise.autoinsure.ws/";
         SoapObject request = new SoapObject(NAMESPACE, method);
         int paramCounter = 0;
         for(String arg : args){
@@ -44,8 +47,10 @@ public class AsyncWebServiceCaller extends AsyncTask<String, Void, String> {
         }
         SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(SoapEnvelope.VER11);
         envelope.setOutputSoapObject(request);
+        String URL = "http://10.0.2.2:8080/AutoInSureWS?WSDL";
         HttpTransportSE androidHttpTransport = new HttpTransportSE(URL);
-        String SOAP_ACTION = "\"" + NAMESPACE + serviceName + "/" + method + "\"";
+        String SERVICE_NAME = "AutoInsureWS";
+        String SOAP_ACTION = "\"" + NAMESPACE + SERVICE_NAME + "/" + method + "\"";
         Log.i("AWSC.makeRequest", SOAP_ACTION);
         try {
             androidHttpTransport.call(SOAP_ACTION, envelope);
