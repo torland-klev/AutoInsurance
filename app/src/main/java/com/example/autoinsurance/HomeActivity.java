3a8403deea5520cc3fb5e4d4a93417be7cb0d277
torland-klev <torland-klev@users.noreply.github.com>
2019-03-07 21:39:48 +0100

    Fixed some layout-stuff in home

--- app/src/main/java/com/example/autoinsurance/HomeActivity.java
+++ app/src/main/java/com/example/autoinsurance/HomeActivity.java
@@ -6,6 +6,7 @@ import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
@@ -81,22 +82,46 @@ public class HomeActivity extends AppCompatActivity implements AsyncResponse{
         RelativeLayout rl = findViewById(R.id.home_layout);
         int c = 0;
         for (Map.Entry<String, String> pair : customer.entrySet()) {
+
+            //C is used for TextView ID's
             c++;
+
+            //Declare new TextViews
             TextView value = new TextView(this);
             TextView key = new TextView(this);
+
+            //Set properties for value
             value.setText(pair.getValue());
             value.setId(c);
             value.setGravity(Gravity.CENTER_HORIZONTAL);
-            //key.setTypeface(null, Typeface.BOLD);
+
+            //Set properties for key
+            key.setTypeface(null, Typeface.BOLD);
             key.setText(pair.getKey());
+
+            //Set layout params
             RelativeLayout.LayoutParams p = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
             RelativeLayout.LayoutParams p2 = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
             if (c == 1){
                 p.addRule(RelativeLayout.BELOW, R.id.home_title);
+                p2.addRule(RelativeLayout.BELOW, R.id.home_title);
             } else {
                 p.addRule(RelativeLayout.BELOW, c-1);
+                p2.addRule(RelativeLayout.BELOW, c-1);
             }
-            p2.addRule(RelativeLayout.ALIGN_LEFT, c);
+            p.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+            p2.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
+
+            int size_dp = 8;
+
+            int dp = (int) TypedValue.applyDimension(
+                    TypedValue.COMPLEX_UNIT_DIP, size_dp, getResources()
+                            .getDisplayMetrics());
+
+            p.setMargins(dp, dp,dp*2,dp);
+            p2.setMargins(dp*2, dp,dp,0);
+
+            //Set layout params
             value.setLayoutParams(p);
             key.setLayoutParams(p2);
             rl.addView(value);
