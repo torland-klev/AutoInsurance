ca41459252070ee83b60880c000ff9067431ea9e
torland-klev <torland-klev@users.noreply.github.com>
2019-03-14 10:58:47 +0100

    Added layout to New Claim, no functionality yet

--- app/src/main/java/com/example/autoinsurance/NewClaimActivity.java
+++ app/src/main/java/com/example/autoinsurance/NewClaimActivity.java
@@ -8,7 +8,6 @@ public class NewClaimActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_new_claim);
     }
 }
